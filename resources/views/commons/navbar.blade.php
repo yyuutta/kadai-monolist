ca92d4b8b72c9b6106281dced0adbd69c376c7ad
yyuutta <ayaragi.yamayu@gmail.com>
2018-08-01 08:02:43 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,8 +12,36 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ route('signup.get') }}">新規登録</a></li>
-                    <li><a href="#">ログイン</a></li>
+                    @if (Auth::check())
+                        <li>
+                            <a href="#">
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                アイテムを追加
+                              </a>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="gravatar">
+                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
+                                </span>
+                                {{ Auth::user()->name }}
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#">マイページ</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="{{ route('logout.get') }}">ログアウト</a>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li><a href="{{ route('signup.get') }}">新規登録</a></li>
+                        <li><a href="{{ route('login') }}">ログイン</a></li>
+                    @endif
                 </ul>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <div class="cover-inner">
             <div class="cover-contents">
                 <h1>素敵なモノと出会う場所</h1>
-                <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+                @if (!Auth::check())
+                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
+                @endif
             </div>
         </div>
     </div>
