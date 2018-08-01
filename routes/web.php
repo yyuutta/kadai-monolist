b39e63d36840d3251c400f32f82d355882b15629
yyuutta <ayaragi.yamayu@gmail.com>
2018-08-01 07:38:09 +0000

    registration

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
